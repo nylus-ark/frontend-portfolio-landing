3a22ea77b4a3ba736b513b419705db2cfba49b1f
Pavel <krasulinpavel1@yandex.ru>
2023-05-20 23:55:16 +0400

    Add subscribe form submission

---
+++ source/js/form/form.js
@@ -0,0 +1,38 @@
+const subscribeForm = document.forms.subscribeForm;
+
+const modalSuccess = document.querySelector('.modal-success');
+const modalOverlay = document.querySelector('.modal-overlay');
+const modalText = modalSuccess?.querySelector('.modal-text');
+
+const body = document.body;
+
+function sendForm(form, modal) {
+  const handleForm = (evt) => {
+    evt.preventDefault();
+
+    const data = new FormData(form);
+    const dataEntries = data?.entries();
+    const inputs = [];
+
+    [...dataEntries]?.forEach(([key, value]) => {
+      console.log(`${key}: ${value}`);
+      inputs.push({ [key]: value });
+    });
+
+    if (modalText) {
+      modalText.innerText = `Your data successfully send to ${inputs[0].email}`;
+    }
+
+    body.classList.add('modal-open');
+    modal?.classList.add('modal--show');
+    modalOverlay?.classList.add('modal-overlay--show');
+
+    setTimeout(() => {
+      removeClassModal(modal);
+    }, 5000);
+  };
+
+  form?.addEventListener('submit', handleForm);
+}
+
+sendForm(subscribeForm, modalSuccess);
