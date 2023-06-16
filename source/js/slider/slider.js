47b49cde8e00330a8e9c9d80a504e48cec54f0b8
Pavel <krasulinpavel1@yandex.ru>
2023-06-16 23:26:58 +0400

    Add testimonials slider

--- source/js/slider/slider.js
+++ source/js/slider/slider.js
@@ -1,8 +1,26 @@
-const swiper = new Swiper('.swiper.services-slider__swiper', {
-  loop: false,
+const servicesSlider = document.querySelector('.services-slider__swiper');
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+if (servicesSlider) {
+  const servicesSwiper = new Swiper('.services-slider__swiper', {
+    loop: false,
+    spaceBetween: 10,
+
+    navigation: {
+      nextEl: '.services-slider__btn-next',
+      prevEl: '.services-slider__btn-prev',
+    },
+  });
+}
+
+const testimonialsSlider = document.querySelector('.testimonials-slider');
+
+if (testimonialsSlider) {
+  const testimonialsSwiper = new Swiper('.testimonials-slider', {
+    loop: false,
+
+    navigation: {
+      nextEl: '.testimonials__btn-next',
+      prevEl: '.testimonials__btn-prev',
+    },
+  });
+}
